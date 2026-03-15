78c8ede182586b654b2146edf6418355799d0df8
dunsqui14 <dmayorlopez2007@gmail.com>
2026-03-15 19:59:33 +0100

    ficheros trabajo

--- cuentas.py
+++ cuentas.py
@@ -1,26 +1,29 @@
 from abc import ABC, abstractmethod
 
 class Cuenta (ABC):
+    #Creamos la clase abstracta  "Cuenta", para definir el comportamiento de cualquier cuenta '''
     def __init__(self, nombre, saldo_inicial: float = 0.0):
         self.nombre = nombre
+        #Aquí lo que hacemos es proteger el saldo y el historial haciendolos privados
         self._saldo_actual = saldo_inicial
         self._historial_transacciones = []
 
     @property
     def saldo(self):
+        #Aquí podemos mirar el saldo, pero no podremos modificarlo
         return self._saldo_actual
 
     @abstractmethod
     def obtener_tipo(self):
-
+        # Utilizando en abstractmethod obligamos a las clases hijas a que implementen este metodo (cada una el suyo propio)
         pass
 
     def registrar_transaccion(self,cantidad: float, descripcion: str = ""):
-
+        #Este metodo suma o resta dinero al saldo protegido y guarda el registro
         self._saldo_actual += cantidad
         self._historial_transacciones.append(f'{descripcion} : {cantidad}')
 
-    def transferir(self, cantidad, cuenta_destino):
+    def transferir(self, cantidad, cuenta_destino): # Realiza una transferencia a otra cuenta validando el saldo
         if self._saldo_actual >= cantidad:
 
             self.registrar_transaccion(f"Transferencia enviada a {cuenta_destino.nombre}", -cantidad)
@@ -32,6 +35,7 @@ class Cuenta (ABC):
 
 
 class CuentaAhorro(Cuenta):
+    # Creamo sun objeto cuenta ahorro
     def __init__(self, nombre: str, saldo_inicial: float, objetivo_ahorro: float):
         super().__init__(nombre, saldo_inicial)
         self.objetivo_ahorro = objetivo_ahorro
@@ -40,6 +44,7 @@ class CuentaAhorro(Cuenta):
         return "Cuenta Ahorro"
 
 class CuentaPrincipal(Cuenta):
+    # Y otro cuenta principal, la cuenta corriente
     def __init__(self, nombre: str, saldo_inicial: float):
         super().__init__(nombre, saldo_inicial)
 
